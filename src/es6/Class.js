708940ea821bef14a9549dc2a4530ef6f404bddd
David Maser <davidemaser@gmail.com>
2017-09-28 10:27:09 -0400

    Class extend

---
+++ src/es6/Class.js
@@ -0,0 +1,28 @@
+/**
+ * Created by David Maser on 27/09/2017.
+ */
+class Welcome{
+  /**
+   * Creates a welcome message
+   * @param log {String[]} Log message
+   * @param obj {Object[]} Formatted array of objects
+   * @param str {String} Another string
+   */
+  constructor(log,obj,str){
+
+  }
+
+  /**
+   * gets the welcome message
+   * @return {Object} the welcome object
+   */
+  run(){
+
+  }
+}
+/**
+ * @extends Welcome
+ */
+class Hello extends Welcome{
+
+}
